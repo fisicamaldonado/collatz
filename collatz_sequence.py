55071d3b92347feddf02a9dffd9077477df83a4e
Pablo Vaz <64449448+fisicamaldonado@users.noreply.github.com>
2023-06-07 16:58:27 -0300

    Update collatz_sequence.py

--- collatz_sequence.py
+++ collatz_sequence.py
@@ -1,15 +1,33 @@
+def print_collatz_sequence(starting_number, sequence, iterations, max_value):
+    """
+    Prints the Collatz sequence and related information.
+    """
+    print('The Collatz Sequence for number', starting_number, 'is:', sequence)
+    print('Max value in the sequence is', max_value)
+    print('There are', iterations, 'iterations in this sequence')
+    print('The ratio between the # of Iterations and the starting number is', iterations / starting_number)
+    print('The ratio between the max value and the starting number is', max_value / starting_number)
+
 def collatz_sequence(starting_number):
     """
     Calculates the Collatz sequence for a given starting number.
     """
-    sequence = [starting_number]
-    while starting_number != 1:
-        if starting_number % 2 == 0: #if the starting number is even, divide by 2 (N/2).
-            starting_number = starting_number // 2
-        else:
-            starting_number = 3 * starting_number + 1 #if the starting number is odd, multiply by 3 and add 1 (3N + 1).
-        sequence.append(starting_number)
-    return sequence
+    if not isinstance(starting_number, int) or starting_number <= 0:
+        raise ValueError("Input must be a positive integer.")
 
-n = 10
-print(collatz_sequence(n))
+    sequence = [starting_number]
+    current_number = starting_number
+
+    while current_number != 1:
+        if current_number % 2 == 0:
+            current_number //= 2
+        else:
+            current_number = 3 * current_number + 1
+        sequence.append(current_number)
+
+    iterations = len(sequence) - 1
+    max_value = max(sequence)
+
+    print_collatz_sequence(starting_number, sequence, iterations, max_value)
+
+collatz_sequence(27) #I don't use an input here because is common to use Jupyter Notebooks and it's better with direct inputs in the script.
